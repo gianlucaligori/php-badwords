1e840b07b2f21e8a9427af57851082d50053fa70
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-06-06 17:17:12 +0200

    style to be added

--- index.php
+++ index.php
@@ -1,17 +1,47 @@
+<?php
+// Ottenere il testo inserito dall'utente
+$text = $_POST['paragrafo'];
+
+// Definire Parola censurata
+$censoredWord = $_POST['bannedWord'];
+
+// Sostituzione parola da censurare con xxx di Subject $text
+$censoredText = str_replace($censoredWord, 'xxx', $text);
+
+
+// Ottenere la lunghezza della parola
+$wordLen = strlen($censoredText); ?>
+
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <?php echo "funziona";
-    phpinfo() ?>
-    <h1>Prova</h1>
+    <div class="container">
+
+        <div class="typedPar">
+            <h1> Il tuo testo è: <?= $text ?> </h1>
+        </div>
+
+        <div>
+            <h2>Parola Censurata: <span class="cenWord"><?= $censoredWord ?></span></h2>
+        </div>
+
+        <div class="cenText">
+            <h2> Testo con parole censurate <?= $censoredText ?> </h2>
+        </div>
+
+        <div>
+            <h2>Lunghezza della Parola: <?= $wordLen ?></h2>
+        </div>
+    </div>
 </body>
 
 </html>
